aa27f1d73390c7f8fdd0f4b868c44126d016682b
Ivan Pinojtić <ipinojtic@tvz.hr>
2018-03-19 20:29:44 +0100

    Add project files.

---
+++ WebOrder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebOrder.Startup))]
+namespace WebOrder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
